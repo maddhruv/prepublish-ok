ea6aea85564fc61f473b9cec0955cc714dd5d817
Dhruv Jain <dhruvjainpenny@gmail.com>
2019-08-23 12:45:17 +0530

    fix the command

--- npm-audit.js
+++ npm-audit.js
@@ -15,7 +15,7 @@ const totalVulnerabilities = low + moderate + high + critical
 
 if (totalVulnerabilities > 0) {
   console.log(`${error} ${chalk.red('Your project has', totalVulnerabilities, 'vulnerabilities, please fix them and then publish')}`)
-  console.log(chalk.yellow('You can simple run `npm audit --fix` to fix them'))
+  console.log(chalk.yellow('You can simple run `npm audit fix` to fix them'))
   process.exit(1)
 } else {
   console.log(`${success} Wohoo! Your project doesn't has any vulnerabilities`)
